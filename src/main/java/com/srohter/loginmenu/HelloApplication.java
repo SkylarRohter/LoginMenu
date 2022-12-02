70cb41a1bfd882e06d6b7fa2c6a717091c234535
ddad69 <skyriversub@gmail.com>
2022-12-01 23:42:58 -0800

    Second Commit

--- src/main/java/com/srohter/loginmenu/HelloApplication.java
+++ src/main/java/com/srohter/loginmenu/HelloApplication.java
@@ -4,16 +4,32 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    private double xOffset;
+    private double yOffset;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        String css = this.getClass().getResource("style.css").toExternalForm();
+        scene.getStylesheets().add(css);
         stage.setScene(scene);
+        scene.setOnMousePressed(event -> {
+            xOffset = stage.getX() - event.getScreenX();
+            yOffset = stage.getY() - event.getScreenY();
+        });
+        //Lambda mouse event handler
+        scene.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() + xOffset);
+            stage.setY(event.getScreenY() + yOffset);
+        });
+        stage.setResizable(false);
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
     }
 
--- src/main/java/com/srohter/loginmenu/HelloController.java
+++ src/main/java/com/srohter/loginmenu/HelloController.java
@@ -1,14 +1,43 @@
 package com.srohter.loginmenu;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private TextField emailBox;
+    @FXML
+    private PasswordField passwordBox;
+    @FXML
+    private CheckBox rememberBox;
+    @FXML
+    private Label rememberLabel;
+    @FXML
+    private Label forgotPassword;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Label preUser;
+    @FXML
+    private Label prePass;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onLoginButtonClick(){
+        
+    }
+    @FXML
+    protected void prePassEnter(){
+        prePass.setDisable(true);
+        System.out.println("1");
+    }
+    @FXML
+    protected void prePassExit(){
+        prePass.setDisable(false);
+        System.out.println("2");
     }
 }
